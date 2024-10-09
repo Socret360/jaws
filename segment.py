eb68708c3045cbced26cbe3999c14f3b0bcfbcf5
Socret360 <lee.socret@gmail.com>
2024-10-09 18:37:42 +0700

    added option to segment.py for taking inputs from and producing output to the console

--- segment.py
+++ segment.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 #
 from utils import preprocess_phylypo_sample
@@ -9,15 +10,27 @@ parser.add_argument('config', type=str, help='Path to config file.')
 parser.add_argument('model_path', type=str, help='Path to model weight file.')
 parser.add_argument('sample', type=str,
                     help='Path to the input text file containing the text to segment.')
+parser.add_argument('--file_mode', action='store_true',
+                    help='Wether sample is a file')
+parser.add_argument('--console_output', action='store_true',
+                    help='Whether to output the result to console')
 
 args = parser.parse_args()
 
 model = JAWSModel(args.config)
 model.load(args.model_path)
 
-with open(args.sample, 'r') as input_file:
-    preprocessed, _ = preprocess_phylypo_sample(input_file.read())
-    out = model.segment(preprocessed)
+if args.file_mode:
+    with open(args.sample, 'r') as input_file:
+        input_str = input_file.read()
+else:
+    input_str = args.sample
 
-with open(args.sample.replace(".txt", ".seg.txt"), "w") as outfile:
-    outfile.write(out)
+preprocessed, _ = preprocess_phylypo_sample(input_str)
+out = model.segment(preprocessed)
+
+if not args.console_output:
+    with open(args.sample.replace(".txt", ".seg.txt"), "w") as outfile:
+        outfile.write(out)
+else:
+    print(out)
